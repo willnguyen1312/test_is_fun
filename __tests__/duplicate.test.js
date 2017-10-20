dd8d717d64c7a694c1cf464cba4fa1454dcdd95d
Nam Nguyen <nam.nguyen.node@outlook.com>
2017-10-20 11:03:26 +0700

    Better duplicate functions

--- __tests__/duplicate.test.js
+++ __tests__/duplicate.test.js
@@ -1,4 +1,5 @@
 import { getDuplicated } from "../duplicate"
+
 const arr = [1, 1, 1, 3, 2, 3, 2, 4, 1, 2]
 
 describe("Duplicate", () => {
--- duplicate.js
+++ duplicate.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line
 export const getDuplicated = (array) => {
+// const getDuplicated = (array) => {
     const counting = {}
     const result = new Set()
     for (const item of array) {
@@ -11,3 +13,14 @@ export const getDuplicated = (array) => {
 
     return result;
 }
+
+const getCouting = array => array.reduce((acc, curr) => {
+    if (!acc[curr]) {
+        acc[curr] = 1
+    } else {
+        acc[curr] += 1
+    }
+    return acc
+}, {})
+
+// console.log(getCouting([1,2,3,2,3,1,1,9]));
